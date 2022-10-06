c5645e8c7b9b2f0964c7ad5d0151ac95b1e892b2
Antonio Aversa <differenziale@gmail.com>
2022-10-06 20:11:07 +0200

    Exercises 1st and 2nd chapter

---
+++ CTCI/Exercises1.cs
@@ -0,0 +1,185 @@
+﻿namespace CTCI;
+
+public static class Exercises1
+{
+    public static void Ex2_Reverse(char[] s)
+    {
+        RReverse(s, 0);
+    }
+
+    private static (int, char) RReverse(char[] s, int i)
+    {
+        if (s.Length <= i) throw new Exception("Malformed string");
+        if (s[i] == '\0') return (i, '\0');
+        var (N, v) = RReverse(s, i + 1);
+        if (i < N / 2)
+        {
+            s[i] = v;
+            return (N, '\0');
+        }
+        else
+        {
+            var t = s[i];
+            s[i] = s[N - 1 - i];
+            return (N, t);
+        }
+    }
+
+    public static IEnumerable<char> Ex3_RemoveDuplicates(char[] s)
+    {
+        for (var i = 0; i < s.Length; i++)
+        {
+            if (s[i] == '\0')
+                continue;
+
+            yield return s[i];
+
+            for (var j = i + 1; j < s.Length; j++)
+                if (s[i] == s[j])
+                    s[j] = '\0';
+        }
+    }
+
+    public static void Ex3_RemoveDuplicatesInPlace(char[] s)
+    {
+        var next = 0;
+        for (var i = 0; i < s.Length; i++)
+        {
+            if (s[i] == '\0')
+                continue;
+
+            s[next] = s[i];
+            next++;
+
+            for (var j = next; j < s.Length; j++)
+                if (s[i] == s[j])
+                    s[j] = '\0';
+        }
+    }
+
+    public static bool Ex4_AreAnagrams(string s1, string s2)
+    {
+        if (s1.Length != s2.Length)
+            return false;
+        var s1Occurrences = GetOccurrences(s1);
+        foreach (var c in s2)
+        {
+            if (!s1Occurrences.TryGetValue(c, out var i) || i == 0)
+                return false;
+            s1Occurrences[c] = i - 1;
+        }
+        return true;
+    }
+
+    private static IDictionary<char, int> GetOccurrences(string s)
+    {
+        var occurrences = new Dictionary<char, int> { };
+        foreach (var c in s)
+        {
+            if (!occurrences.TryGetValue(c, out var i))
+                occurrences[c] = 1;
+            else occurrences[c] = i + 1;
+        }
+
+        return occurrences;
+    }
+
+    public static IEnumerable<char> Ex5_ReplaceSpaces(string s)
+    {
+        foreach (var c in s)
+        {
+            if (c == ' ')
+            {
+                yield return '%';
+                yield return '2';
+                yield return '0';
+            }
+            else
+            {
+                yield return c;
+            }
+        }
+    }
+
+    public static void Ex5_ReplaceSpacesInPlace(char[] s)
+    {
+        var length = 0;
+        var numberOfSpaces = 0;
+        foreach (var c in s) 
+        {
+            if (c == '\0') break;
+            if (c == ' ') numberOfSpaces++;
+            length++;
+        }
+
+        var j = length + numberOfSpaces * 2;
+        if (s.Length <= j) throw new ArgumentException($"{nameof(s)} is not big enough");
+        for (var i = length; i >= 0; i--)
+        {
+            if (s[i] == ' ')
+            {
+                s[j--] = '0';
+                s[j--] = '2';
+                s[j--] = '%';
+            }
+            else s[j--] = s[i];
+        }
+    }
+
+    public static void Ex6_RotateMatrix(int[,] m)
+    {
+        var n = m.GetLength(0);
+        for (var i = 0; i < n / 2; i++)
+        {
+            for (var j = i; j < n - 1 - i; j++)
+            {
+                var (v1, v2, v3, v4) = (m[i, j], m[j, n - 1 - i], m[n - 1 - i, n - 1 -i - j], m[n - 1 - i - j, i]);
+                (m[i, j], m[j, n - 1 - i], m[n - 1 - i, n - 1 - i - j], m[n - 1 - i - j, i]) = (v2, v3, v4, v1);
+            }
+        }
+    }
+
+    public static void Ex7_ResetRowAndColumn(int[,] x)
+    {
+        var m = x.GetLength(0);
+        var n = x.GetLength(1);
+        var zeroInRow = new bool[n];
+        var zeroInColumn = new bool[m];
+        for (var i = 0; i < m; i++)
+        {
+            zeroInRow[i] = false;
+            for (var j = 0; j < n; j++)
+            {
+                if (x[i, j] == 0)
+                {
+                    zeroInRow[i] = true;
+                    break;
+                }
+            }
+        }
+
+        for (var j = 0; j < n; j++)
+        {
+            zeroInColumn[j] = false;
+            for (var i = 0; i < m; i++)
+            {
+                if (x[i, j] == 0)
+                {
+                    zeroInColumn[i] = true;
+                    break;
+                }
+            }
+        }
+
+        for (var i = 0; i < m; i++)
+            for (var j = 0; j < n; j++)
+                if (zeroInRow[i] || zeroInColumn[j])
+                    x[i, j] = 0;
+    }
+
+    public static bool Ex8_IsRotation(string s1, string s2)
+    {
+        if (s1.Length != s2.Length) return false;
+        return ((s1 + s1).Contains(s2));
+    }
+}
---
+++ CTCI/Exercises2.cs
@@ -0,0 +1,169 @@
+﻿namespace CTCI;
+
+public static class Exercises2
+{
+    public static void Ex1_RemoveDuplicates<T>(LinkedList<T> list)
+    {
+        var distinctItems = new HashSet<T>();
+        for (var item = list.First; item != null; item = item.Next)
+        {
+            if (distinctItems.Contains(item.Value))
+            {
+                var toRemove = item;
+                item = item.Previous;
+                list.Remove(toRemove);
+            }
+            else
+                distinctItems.Add(item.Value);
+        }
+    }
+
+    public static void Ex1_RemoveDuplicatesNoBuffer<T>(LinkedList<T> list)
+    {
+        for (var i1 = list.First; i1 != null; i1 = i1.Next)
+        {
+            var i2 = i1.Next;
+            while (i2 != null)
+            {
+                if (Equals(i1.Value, i2.Value))
+                {
+                    var toRemove = i2;
+                    i2 = i2.Next;
+                    list.Remove(toRemove);
+                }
+                else
+                    i2 = i2.Next;
+            }
+        }
+    }
+
+    public static (T?, bool) Ex2_FindNthToLast<T>(LinkedList<T> list, int n)
+    {
+        if (n < 1)
+            throw new ArgumentException(nameof(n));
+
+        int i;
+        LinkedListNode<T>? current;
+        for (i = 0, current = list.First; i < n && current != null; i++, current = current.Next) ;
+
+        if (i < n)
+            return (default, false);
+
+        var result = list.First;
+        while (current != null)
+        {
+            current = current.Next;
+            result = result!.Next;
+        }
+
+        return (result!.Value, true);
+    }
+
+    public class LinkedNode<T>
+    {
+        public T? Value { get; set; }
+        public LinkedNode<T>? Next { get; set; }
+
+        public override string ToString()
+        {
+            return RecursiveToString(0);
+        }
+
+        private string RecursiveToString(int level)
+        {
+            if (level > 10)
+                return "...";
+            return $"{Value}{(Next != null ? " " + Next.RecursiveToString(level + 1) : "")}";
+        }
+    }
+
+    public static void Ex3_DeleteNode<T>(LinkedNode<T> node)
+    {
+        var next = node.Next;
+        if (next == null)
+            throw new Exception("Last node of the list: cannot remove it");
+        node.Value = next.Value;
+        node.Next = next.Next;
+        next.Value = default;
+        next.Next = null;
+    }
+
+    public static LinkedList<byte> Ex4_Sum(LinkedList<byte> n1, LinkedList<byte> n2)
+    {
+        byte reminder = 0;
+        var sum = new LinkedList<byte>();
+        var i1 = n1.First;
+        var i2 = n2.First;
+        while (i1 != null || i2 != null)
+        {
+            var digitsSum = (i1 != null ? i1.Value : 0) + (i2 != null ? i2.Value : 0) + reminder;
+            sum.AddLast((byte)(digitsSum % 10));
+            reminder = (byte)(digitsSum / 10);
+            i1 = i1?.Next;
+            i2 = i2?.Next;
+        }
+
+        if (reminder != 0)
+            sum.AddLast(reminder);
+
+        return sum;
+    }
+
+    public static LinkedNode<T>? Ex5_FindLoop<T>(LinkedNode<T>? list)
+    {
+        var alreadyVisited = new HashSet<LinkedNode<T>> { };
+        var current = list;
+        while (current != null && !alreadyVisited.Contains(current))
+        {
+            alreadyVisited.Add(current);
+            current = current.Next;
+        }
+        return current;
+    }
+
+    public static LinkedNode<T>? Ex5_FindLoop_NoHashSet<T>(LinkedNode<T>? head)
+    {
+        if (head == null) return null;
+
+        var fast = head.Next;
+        var slow = head;
+
+        LinkedNode<T>? loopingNode = null;
+        int loopLength = 0;
+        while (slow != null && fast != null)
+        {
+            if (slow == fast)
+            {
+                loopingNode = slow;
+                break;
+            }
+
+            slow = slow.Next;
+            fast = fast.Next;
+
+            if (slow == fast)
+            {
+                loopingNode = slow;
+                break;
+            }
+            fast = fast?.Next;
+            loopLength += 2;
+        }
+
+        if (loopingNode == null) 
+            return null;
+
+        var current = head;
+        while (true)
+        {
+            for (var i = 0; i <= loopLength; i++)
+            {
+                if (current == loopingNode)
+                    return current;
+                loopingNode = loopingNode.Next;
+            }
+
+            current = current.Next;
+        }
+    }
+}
