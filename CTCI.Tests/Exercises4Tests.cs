f9dd7c76be4d578525d148fb4b7c00e8af61c6ac
Antonio Aversa <differenziale@gmail.com>
2022-10-12 17:19:05 +0200

    Exercises 4th chapter

---
+++ CTCI.Tests/Exercises4Tests.cs
@@ -0,0 +1,323 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections;
+using System.ComponentModel.DataAnnotations;
+using static CTCI.Exercises4;
+
+namespace CTCI.Tests;
+
+[TestClass]
+public class Exercises4Tests
+{
+    [TestMethod]
+    public void Ex1_TreeIsBalanced()
+    {
+        var tree = new Node(new() { });
+        Assert.IsTrue(Exercises4.Ex1_TreeIsBalanced(tree));
+
+        tree.Children.Add(new Node(new() { }));
+        Assert.IsTrue(Exercises4.Ex1_TreeIsBalanced(tree));
+        tree.Children.Add(new Node(new() { }));
+        Assert.IsTrue(Exercises4.Ex1_TreeIsBalanced(tree));
+
+        tree.Children[0].Children.Add(new Node(new() { }));
+        Assert.IsTrue(Exercises4.Ex1_TreeIsBalanced(tree));
+
+        tree.Children[0].Children[0].Children.Add(new Node(new() { }));
+        Assert.IsFalse(Exercises4.Ex1_TreeIsBalanced(tree));
+
+        tree.Children[1].Children.Add(new Node(new() { }));
+        Assert.IsTrue(Exercises4.Ex1_TreeIsBalanced(tree));
+
+        tree.Children[1].Children[0].Children.Add(new Node(new() { }));
+        Assert.IsTrue(Exercises4.Ex1_TreeIsBalanced(tree));
+
+        tree.Children[1].Children[0].Children[0].Children.Add(new Node(new() { }));
+        Assert.IsTrue(Exercises4.Ex1_TreeIsBalanced(tree));
+
+        tree.Children[1].Children[0].Children[0].Children[0].Children.Add(new Node(new() { }));
+        Assert.IsFalse(Exercises4.Ex1_TreeIsBalanced(tree));
+    }
+
+    [TestMethod]
+    public void Ex2_GraphRoute()
+    {
+        var n1 = new Node(new() { });
+        var n2 = new Node(new() { });
+        var n3 = new Node(new() { });
+        var n4 = new Node(new() { });
+        var n5 = new Node(new() { });
+
+        Assert.IsTrue(Exercises4.Ex2_GraphRouteDfs(n1, n1));
+        Assert.IsFalse(Exercises4.Ex2_GraphRouteDfs(n1, n2));
+
+        n1.Children.Add(n2);
+        Assert.IsTrue(Exercises4.Ex2_GraphRouteDfs(n1, n2));
+        Assert.IsTrue(Exercises4.Ex2_GraphRouteDfs(n2, n1));
+
+        n2.Children.Add(n3);
+        Assert.IsTrue(Exercises4.Ex2_GraphRouteDfs(n2, n3));
+        Assert.IsTrue(Exercises4.Ex2_GraphRouteDfs(n1, n3));
+        Assert.IsTrue(Exercises4.Ex2_GraphRouteDfs(n3, n1));
+        Assert.IsFalse(Exercises4.Ex2_GraphRouteDfs(n1, n4));
+
+        n2.Children.Add(n4);
+        Assert.IsFalse(Exercises4.Ex2_GraphRouteDfs(n3, n4));
+        Assert.IsTrue(Exercises4.Ex2_GraphRouteDfs(n1, n3));
+        Assert.IsTrue(Exercises4.Ex2_GraphRouteDfs(n1, n4));
+    }
+
+    [TestMethod]
+    public void Ex3_BSTFromSortedList()
+    {
+        var list = new List<int> { 1, 2, 4, 5, 6, 8, 10, 11 };
+        var tree = Exercises4.Ex3_BSTFromSortedList(list);
+        Assert.IsTrue(IsBST(tree));
+        Assert.IsTrue(IsBalanced(tree));
+    }
+
+    private static bool IsBST<T>(BSTNode<T> node)
+        where T : IComparable<T>
+    {
+        return RIsBST(node).Item1;
+
+        static (bool, int, int) RIsBST(BSTNode<T>? node)
+        {
+            if (node == null) return (true, int.MaxValue, int.MinValue);
+            
+            var (bstLeft, minLeft, maxLeft) = RIsBST(node.Left);
+            if (!bstLeft) return (false, int.MinValue, int.MaxValue);
+
+            var (bstRight, minRight, maxRight) = RIsBST(node.Right);
+            if (!bstRight) return (false, int.MinValue, int.MaxValue);
+
+            var bst = maxLeft.CompareTo(node.Value) <= 0 && minRight.CompareTo(node.Value) >= 0;
+            return (bst, minLeft, maxRight);
+        }
+    }
+
+    private static bool IsBalanced<T>(BSTNode<T> node)
+    {
+        return MaxHeight(node) - MinHeight(node) <= 1;
+        
+        static int MaxHeight(BSTNode<T>? node)
+        {
+            if (node == null) return 0;
+            return Math.Max(MaxHeight(node.Left), MaxHeight(node.Right)) + 1;
+        }
+
+        static int MinHeight(BSTNode<T>? node)
+        {
+            if (node == null) return 0;
+            return Math.Min(MinHeight(node.Left), MinHeight(node.Right)) + 1;
+        }
+    }
+
+    [TestMethod]
+    public void Ex4_Levels()
+    {
+        var list = new List<int> { 1, 2, 4, 5, 6, 8, 10, 11 };
+        var tree = Exercises4.Ex3_BSTFromSortedList(list);
+
+        var levels = Exercises4.Ex4_Levels(tree);
+        Assert.AreEqual(4, levels.Count);
+        Assert.IsTrue(new[] { 5 }.SequenceEqual(levels[0].Select(n => n.Value)));
+        Assert.IsTrue(new[] { 2, 8 }.SequenceEqual(levels[1].Select(n => n.Value)));
+        Assert.IsTrue(new[] { 1, 4, 6, 10 }.SequenceEqual(levels[2].Select(n => n.Value)));
+        Assert.IsTrue(new[] { 11 }.SequenceEqual(levels[3].Select(n => n.Value)));
+    }
+
+    [TestMethod]
+    public void Ex5_NextNode()
+    {
+        var n1 = new BSTNodeWithParent();
+        var n2 = new BSTNodeWithParent();
+        var n3 = new BSTNodeWithParent();
+        var n4 = new BSTNodeWithParent();
+        var n5 = new BSTNodeWithParent();
+        var n6 = new BSTNodeWithParent();
+        var n7 = new BSTNodeWithParent();
+        var n8 = new BSTNodeWithParent();
+
+        n1.SetLeft(n2);
+        n1.SetRight(n6);
+        n2.SetLeft(n3);
+        n2.SetRight(n4);
+        n4.SetLeft(n5);
+        n6.SetLeft(n7);
+        n7.SetRight(n8);
+
+        Assert.AreSame(n7, Exercises4.Ex5_NextNode(n1));
+        Assert.AreSame(n5, Exercises4.Ex5_NextNode(n2));
+        Assert.AreSame(n2, Exercises4.Ex5_NextNode(n3));
+        Assert.AreSame(n1, Exercises4.Ex5_NextNode(n4));
+        Assert.AreSame(n4, Exercises4.Ex5_NextNode(n5));
+        Assert.IsNull(Exercises4.Ex5_NextNode(n6));
+        Assert.AreSame(n8, Exercises4.Ex5_NextNode(n7));
+        Assert.AreSame(n6, Exercises4.Ex5_NextNode(n8));
+    }
+
+    [TestMethod]
+    public void Ex7_IsSubtree()
+    {
+        var t1 = BuildFromPreOrder(new List<int> { 8, 4, 2, 1, 3, 6, 5, 7, 12, 10, 9, 11, 14, 13, 15 });
+        var t2 = BuildFromPreOrder(new List<int> { 6 }); // intermediate
+        var t3 = BuildFromPreOrder(new List<int> { 3 }); // leaf
+        var t4 = BuildFromPreOrder(new List<int> { 8 }); // root
+        var t5 = BuildFromPreOrder(new List<int> { 4, 2 }); // left 2-chain
+        var t6 = BuildFromPreOrder(new List<int> { 4, 6, 7 }); // right 3-chain
+        var t7 = BuildFromPreOrder(new List<int> { 6, 5, 7 }); // bottom balanced 3-nodes tree
+        var t8 = BuildFromPreOrder(new List<int> { 4, 2, 6 }); // intermediate balanced 3-nodes tree
+        var t9 = BuildFromPreOrder(new List<int> { 4, 2, 7 }); // non-existent balanced 3-nodes tree
+
+
+        Assert.IsTrue(Exercises4.Ex7_IsSubtree(t1, t2));
+        Assert.IsTrue(Exercises4.Ex7_IsSubtree(t1, t3));
+        Assert.IsTrue(Exercises4.Ex7_IsSubtree(t1, t4));
+        Assert.IsTrue(Exercises4.Ex7_IsSubtree(t1, t5));
+        Assert.IsTrue(Exercises4.Ex7_IsSubtree(t1, t6));
+        Assert.IsTrue(Exercises4.Ex7_IsSubtree(t1, t7));
+        Assert.IsTrue(Exercises4.Ex7_IsSubtree(t1, t8));
+        Assert.IsFalse(Exercises4.Ex7_IsSubtree(t1, t9));
+    }
+
+    private static BSTNode<T>? BuildFromPreOrder<T>(IList<T> preOrder)
+        where T : IComparable<T>
+    {
+        return RecursiveBuildFromPreOrder(preOrder, 0, default, false).Item1;
+    }
+
+    private static (BSTNode<T>?, int) RecursiveBuildFromPreOrder<T>(IList<T> preOrder, int i, T? max, bool maxDefined)
+        where T : IComparable<T>
+    {
+        if (i >= preOrder.Count)
+            return (null, i);
+        
+        var rootValue = preOrder[i];
+        if (maxDefined && rootValue.CompareTo(max) >= 0)
+            return (null, i);
+
+        var (firstChild, nextIndex1) = RecursiveBuildFromPreOrder<T>(preOrder, i + 1, rootValue, true);
+        var (secondChild, nextIndex2) = RecursiveBuildFromPreOrder<T>(preOrder, nextIndex1, max, maxDefined);
+        return (new BSTNode<T>(rootValue, firstChild, secondChild), nextIndex2);
+    }
+
+    [TestMethod]
+    public void Ex8_PathsSummingToTarget()
+    {
+        var n1 = new BSTNode<int>(1,
+            new BSTNode<int>(1,
+                new BSTNode<int>(0,
+                    new BSTNode<int>(1, null, null),
+                    null),
+                new BSTNode<int>(1,
+                    null,
+                    new BSTNode<int>(2, null, null))),
+            new BSTNode<int>(2,
+                new BSTNode<int>(1, null, null),
+                null));
+        var n2 = n1.Left!;
+        var n3 = n1.Right!;
+        var n4 = n2.Left!;
+        var n5 = n2.Right!;
+        var n6 = n3.Left!;
+        var n7 = n4.Left!;
+        var n8 = n5.Right!;
+
+        var paths1 = Exercises4.Ex8_PathsSummingToTarget(n1, 3);
+        Assert.AreEqual(5, paths1.Count);
+        Assert.IsTrue(paths1.Any(path => path.SequenceEqual(new List<BSTNode<int>> { n1, n2, n4, n7 })));
+        Assert.IsTrue(paths1.Any(path => path.SequenceEqual(new List<BSTNode<int>> { n1, n2, n5 })));
+        Assert.IsTrue(paths1.Any(path => path.SequenceEqual(new List<BSTNode<int>> { n5, n8 })));
+        Assert.IsTrue(paths1.Any(path => path.SequenceEqual(new List<BSTNode<int>> { n1, n3 })));
+        Assert.IsTrue(paths1.Any(path => path.SequenceEqual(new List<BSTNode<int>> { n3, n6 })));
+
+        var paths2 = Exercises4.Ex8_PathsSummingToTarget(n1, 2);
+        Assert.AreEqual(6, paths2.Count);
+        Assert.IsTrue(paths2.Any(path => path.SequenceEqual(new List<BSTNode<int>> { n1, n2 })));
+        Assert.IsTrue(paths2.Any(path => path.SequenceEqual(new List<BSTNode<int>> { n1, n2, n4 })));
+        Assert.IsTrue(paths2.Any(path => path.SequenceEqual(new List<BSTNode<int>> { n2, n4, n7 })));
+        Assert.IsTrue(paths2.Any(path => path.SequenceEqual(new List<BSTNode<int>> { n2, n5 })));
+        Assert.IsTrue(paths2.Any(path => path.SequenceEqual(new List<BSTNode<int>> { n8 })));
+        Assert.IsTrue(paths2.Any(path => path.SequenceEqual(new List<BSTNode<int>> { n3 })));
+    }
+
+    [TestMethod]
+    public void Ex8_PathsSummingToTargetOptimized()
+    {
+        var n1 = new BSTNode<int>(1,
+            new BSTNode<int>(1,
+                new BSTNode<int>(0,
+                    new BSTNode<int>(1, null, null),
+                    null),
+                new BSTNode<int>(1,
+                    null,
+                    new BSTNode<int>(2, null, null))),
+            new BSTNode<int>(2,
+                new BSTNode<int>(1, null, null),
+                null));
+        var n2 = n1.Left!;
+        var n3 = n1.Right!;
+        var n4 = n2.Left!;
+        var n5 = n2.Right!;
+        var n6 = n3.Left!;
+        var n7 = n4.Left!;
+        var n8 = n5.Right!;
+
+        var paths1 = Exercises4.Ex8_PathsSummingToTargetOptimized(n1, 3);
+        Assert.AreEqual(5, paths1.Count);
+        Assert.IsTrue(paths1.Any(path => path.SequenceEqual(new List<BSTNode<int>> { n1, n2, n4, n7 })));
+        Assert.IsTrue(paths1.Any(path => path.SequenceEqual(new List<BSTNode<int>> { n1, n2, n5 })));
+        Assert.IsTrue(paths1.Any(path => path.SequenceEqual(new List<BSTNode<int>> { n5, n8 })));
+        Assert.IsTrue(paths1.Any(path => path.SequenceEqual(new List<BSTNode<int>> { n1, n3 })));
+        Assert.IsTrue(paths1.Any(path => path.SequenceEqual(new List<BSTNode<int>> { n3, n6 })));
+
+        var paths2 = Exercises4.Ex8_PathsSummingToTargetOptimized(n1, 2);
+        Assert.AreEqual(6, paths2.Count);
+        Assert.IsTrue(paths2.Any(path => path.SequenceEqual(new List<BSTNode<int>> { n1, n2 })));
+        Assert.IsTrue(paths2.Any(path => path.SequenceEqual(new List<BSTNode<int>> { n1, n2, n4 })));
+        Assert.IsTrue(paths2.Any(path => path.SequenceEqual(new List<BSTNode<int>> { n2, n4, n7 })));
+        Assert.IsTrue(paths2.Any(path => path.SequenceEqual(new List<BSTNode<int>> { n2, n5 })));
+        Assert.IsTrue(paths2.Any(path => path.SequenceEqual(new List<BSTNode<int>> { n8 })));
+        Assert.IsTrue(paths2.Any(path => path.SequenceEqual(new List<BSTNode<int>> { n3 })));
+    }
+
+    [TestMethod]
+    public void Ex8_PathsSummingToTargetEarly()
+    {
+        var n1 = new BSTNode<int>(1,
+            new BSTNode<int>(1,
+                new BSTNode<int>(0,
+                    new BSTNode<int>(1, null, null),
+                    null),
+                new BSTNode<int>(1,
+                    null,
+                    new BSTNode<int>(2, null, null))),
+            new BSTNode<int>(2,
+                new BSTNode<int>(1, null, null),
+                null));
+        var n2 = n1.Left!;
+        var n3 = n1.Right!;
+        var n4 = n2.Left!;
+        var n5 = n2.Right!;
+        var n6 = n3.Left!;
+        var n7 = n4.Left!;
+        var n8 = n5.Right!;
+
+        var paths1 = Exercises4.Ex8_PathsSummingToTargetEarly(n1, 3);
+        Assert.AreEqual(5, paths1.Count);
+        Assert.IsTrue(paths1.Any(path => path.SequenceEqual(new List<BSTNode<int>> { n1, n2, n4, n7 })));
+        Assert.IsTrue(paths1.Any(path => path.SequenceEqual(new List<BSTNode<int>> { n1, n2, n5 })));
+        Assert.IsTrue(paths1.Any(path => path.SequenceEqual(new List<BSTNode<int>> { n5, n8 })));
+        Assert.IsTrue(paths1.Any(path => path.SequenceEqual(new List<BSTNode<int>> { n1, n3 })));
+        Assert.IsTrue(paths1.Any(path => path.SequenceEqual(new List<BSTNode<int>> { n3, n6 })));
+
+        var paths2 = Exercises4.Ex8_PathsSummingToTargetEarly(n1, 2);
+        Assert.AreEqual(6, paths2.Count);
+        Assert.IsTrue(paths2.Any(path => path.SequenceEqual(new List<BSTNode<int>> { n1, n2 })));
+        Assert.IsTrue(paths2.Any(path => path.SequenceEqual(new List<BSTNode<int>> { n1, n2, n4 })));
+        Assert.IsTrue(paths2.Any(path => path.SequenceEqual(new List<BSTNode<int>> { n2, n4, n7 })));
+        Assert.IsTrue(paths2.Any(path => path.SequenceEqual(new List<BSTNode<int>> { n2, n5 })));
+        Assert.IsTrue(paths2.Any(path => path.SequenceEqual(new List<BSTNode<int>> { n8 })));
+        Assert.IsTrue(paths2.Any(path => path.SequenceEqual(new List<BSTNode<int>> { n3 })));
+    }
+}
---
+++ CTCI/Exercises4.cs
@@ -0,0 +1,243 @@
+﻿using static CTCI.Exercises4;
+
+namespace CTCI;
+
+public class Exercises4
+{
+    public record Node(List<Node> Children);
+
+    public static bool Ex1_TreeIsBalanced(Node node)
+    {
+        return MaxHeight(node) - MinHeight(node) <= 1;
+
+        static int MaxHeight(Node n) => n.Children.Select(c => MaxHeight(c)).DefaultIfEmpty(0).Max() + 1;
+        static int MinHeight(Node n) => n.Children.Select(c => MinHeight(c)).DefaultIfEmpty(0).Min() + 1;
+    }
+
+    public static bool Ex2_GraphRouteDfs(Node n1, Node n2)
+    {
+        return Reachable(n1, n2) || Reachable(n2, n1);
+
+        static bool Reachable(Node from, Node to, HashSet<Node>? alreadyVisited = null)
+        {
+            alreadyVisited ??= new HashSet<Node>();
+
+            if (from == to) return true;
+
+            alreadyVisited.Add(from);
+            foreach (var neighbor in from.Children)
+                if (!alreadyVisited.Contains(neighbor))
+                    if (Reachable(neighbor, to, alreadyVisited))
+                        return true;
+
+            return false;
+        }
+    }
+
+    public record BSTNode<T>(T Value, BSTNode<T>? Left, BSTNode<T>? Right);
+
+    public static BSTNode<T>? Ex3_BSTFromSortedList<T>(IList<T> l)
+    {
+        return RecursiveBSTFromSortedList<T>(l, 0, l.Count - 1);
+    }
+
+    private static BSTNode<T>? RecursiveBSTFromSortedList<T>(IList<T> l, int s, int e)
+    {
+        if (s > e) return null;
+        if (s == e) return new BSTNode<T>(l[s], null, null);
+        var m = s + (e - s) / 2;
+        return new BSTNode<T>(
+            l[m], RecursiveBSTFromSortedList(l, s, m - 1), RecursiveBSTFromSortedList(l, m + 1, e));
+    }
+
+    private record struct NodeAndLevel<T>(BSTNode<T> Node, int Level);
+
+    public static IList<LinkedList<BSTNode<T>>> Ex4_Levels<T>(BSTNode<T>? root)
+    {
+        if (root == null) return Array.Empty<LinkedList<BSTNode<T>>>();
+
+        var levels = new List<LinkedList<BSTNode<T>>>();
+        var nodesQueue = new Queue<NodeAndLevel<T>>();
+        nodesQueue.Enqueue(new(root, 0));
+        while (nodesQueue.Count > 0)
+        {
+            var (node, level) = nodesQueue.Dequeue();
+
+            if (level == levels.Count)
+                levels.Add(new LinkedList<BSTNode<T>>());
+            levels[level].AddLast(node);
+
+            if (node.Left != null) nodesQueue.Enqueue(new(node.Left, level + 1));
+            if (node.Right != null) nodesQueue.Enqueue(new(node.Right, level + 1));
+        }
+
+        return levels;
+    }
+
+    public class BSTNodeWithParent
+    {
+        public BSTNodeWithParent? Parent { get; private set; } = null;
+        public BSTNodeWithParent? Left { get; private set; } = null;
+        public BSTNodeWithParent? Right { get; private set; } = null;
+
+        public void SetLeft(BSTNodeWithParent? left)
+        {
+            Left = left;
+            if (left != null)
+                left.Parent = this;
+        }
+
+        public void SetRight(BSTNodeWithParent? right)
+        {
+            Right = right;
+            if (right != null)
+                right.Parent = this;
+        }
+    };
+
+    public static BSTNodeWithParent? Ex5_NextNode(BSTNodeWithParent? node)
+    {
+        if (node.Right != null)
+        {
+            var next = node.Right;
+            while (next.Left != null)
+                next = next.Left;
+            return next;
+        }
+        if (node.Parent != null)
+        {
+            var next = node;
+            while (next.Parent != null && next.Parent.Right == node)
+                next = next.Parent;
+            return next?.Parent;
+        }
+
+        return null;
+    }
+
+    public static bool Ex7_IsSubtree<T>(BSTNode<T> t1, BSTNode<T> t2)
+        where T : IComparable<T>
+    {
+        return RecursiveIsSubtree(t1, t2);
+    }
+
+    private static bool RecursiveIsSubtree<T>(BSTNode<T>? t1, BSTNode<T>? t2)
+        where T : IComparable<T>
+    {
+        if (t1 == null && t2 == null) return true;
+        if (t1 == null) return false;
+        if (t2 == null) return true;
+
+        if (t1.Value.CompareTo(t2.Value) == 0)
+        {
+            if (RecursiveMatchSubtrees(t1, t2))
+                return true;
+        }
+        if (RecursiveIsSubtree(t1.Left, t2))
+            return true;
+        if (RecursiveIsSubtree(t1.Right, t2))
+            return true;
+        return false;
+    }
+
+    private static bool RecursiveMatchSubtrees<T>(BSTNode<T>? t1, BSTNode<T>? t2)
+        where T : IComparable<T>
+    {
+        if (t1 == null && t2 == null) return true;
+        if (t1 == null) return false;
+        if (t2 == null) return true;
+
+        return t1.Value.CompareTo(t2.Value) == 0 &&
+            RecursiveMatchSubtrees(t1.Left, t2.Left) &&
+            RecursiveMatchSubtrees(t1.Right, t2.Right);
+    }
+
+    public static IList<IList<BSTNode<int>>> Ex8_PathsSummingToTarget(BSTNode<int> n, int t)
+    {
+        return RecursivePathsSummingToTarget(n, t).ToList();
+    }
+
+    private static IEnumerable<IList<BSTNode<int>>> RecursivePathsSummingToTarget(BSTNode<int>? n, int t)
+    {
+        if (n == null)
+            yield break;
+
+        foreach (var p in MathPathSummingToTarget(n, t))
+            yield return p;
+        foreach (var p in RecursivePathsSummingToTarget(n.Left, t))
+            yield return p;
+        foreach (var p in RecursivePathsSummingToTarget(n.Right, t))
+            yield return p;
+    }
+
+    private static IEnumerable<IList<BSTNode<int>>> MathPathSummingToTarget(BSTNode<int> n, int t)
+    {
+        if (n.Value == t)
+            yield return new List<BSTNode<int>> { n };
+
+        if (n.Left != null)
+            foreach (var p in MathPathSummingToTarget(n.Left, t - n.Value))
+                yield return p.Prepend(n).ToList();
+
+        if (n.Right != null)
+            foreach (var p in MathPathSummingToTarget(n.Right, t - n.Value))
+                yield return p.Prepend(n).ToList();
+    }
+
+    public static IList<IList<BSTNode<int>>> Ex8_PathsSummingToTargetOptimized(BSTNode<int> n, int t)
+    {
+        return RecursivePathsSummingToTargetOptimized(n, new List<BSTNode<int>> { }, t).ToList();
+    }
+
+    public static IList<IList<BSTNode<int>>> Ex8_PathsSummingToTargetEarly(BSTNode<int> n, int t)
+    {
+        return DfsWithPaths(n, new List<BSTNode<int>> { }, t).ToList();
+
+        static IList<IList<BSTNode<int>>> DfsWithPaths(BSTNode<int> node, IList<BSTNode<int>> path, int target)
+        {
+            path.Add(node);
+
+            var result = new List<IList<BSTNode<int>>> { };
+
+            var sum = 0;
+            for (var i = path.Count - 1; i >= 0; i--)
+            {
+                sum += path[i].Value;
+                if (sum == target)
+                    result.Add(path.TakeLast(path.Count - i).ToList());
+            }
+
+            if (node.Left != null)
+                result.AddRange(DfsWithPaths(node.Left, path.ToList(), target));
+            if (node.Right != null)
+                result.AddRange(DfsWithPaths(node.Right, path.ToList(), target));
+            return result;
+        }
+    }
+
+    private static IEnumerable<IList<BSTNode<int>>> RecursivePathsSummingToTargetOptimized(
+        BSTNode<int> n, IList<BSTNode<int>> path, int target)
+    {
+        path.Add(n);
+
+        var sum = 0;
+        for (var i = path.Count - 1; i >= 0; i--)
+        {
+            sum += path[i].Value;
+            if (sum == target)
+                yield return path.TakeLast(path.Count - i).ToList();
+        }
+
+        if (n.Left != null)
+        {
+            foreach (var p in RecursivePathsSummingToTargetOptimized(n.Left, path.ToList(), target))
+                yield return p;
+        }
+
+        if (n.Right != null)
+        {
+            foreach (var p in RecursivePathsSummingToTargetOptimized(n.Right, path.ToList(), target))
+                yield return p;
+        }
+    }
+}
