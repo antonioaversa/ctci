81ffc3b2d8301b856a151240b2245cda7482b4d6
Antonio Aversa <differenziale@gmail.com>
2022-10-13 18:57:04 +0200

    Exercises 5th chapter

---
+++ CTCI.Tests/Exercises5Tests.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CTCI.Tests;
+
+[TestClass]
+public class Exercises5Tests
+{
+    [TestMethod]
+    public void Ex1()
+    {
+        Assert.AreEqual(0b10001010100, Exercises5.Ex1(0b10000000000, 0b10101, 2, 6));
+    }
+
+    [TestMethod]
+    public void Ex2()
+    {
+        Assert.AreEqual("1", Exercises5.Ex2("1"));
+        Assert.AreEqual("10", Exercises5.Ex2("2"));
+        Assert.AreEqual("11.1", Exercises5.Ex2("3.5"));
+        Assert.AreEqual("1000000.01", Exercises5.Ex2("64.25"));
+        Assert.AreEqual("10100.11", Exercises5.Ex2("20.75"));
+        Assert.AreEqual("ERROR", Exercises5.Ex2("3.72"));
+    }
+
+    [TestMethod]
+    public void Ex3()
+    {
+        Assert.AreEqual((int.MinValue, 0b10), Exercises5.Ex3(0b1));
+        Assert.AreEqual((0b01, 0b100), Exercises5.Ex3(0b10));
+        Assert.AreEqual((int.MinValue, 0b101), Exercises5.Ex3(0b11));
+        Assert.AreEqual((0b011, 0b110), Exercises5.Ex3(0b101));
+        Assert.AreEqual((0b01111, 0b11011), Exercises5.Ex3(0b10111));
+    }
+
+    [TestMethod]
+    public void Ex4()
+    {
+        Assert.IsTrue(Exercises5.Ex4(0));
+        Assert.IsTrue(Exercises5.Ex4(1));
+        Assert.IsTrue(Exercises5.Ex4(2));
+        Assert.IsFalse(Exercises5.Ex4(3));
+        Assert.IsTrue(Exercises5.Ex4(4));
+        Assert.IsTrue(Exercises5.Ex4(8));
+        Assert.IsFalse(Exercises5.Ex4(15));
+        Assert.IsTrue(Exercises5.Ex4(16));
+        Assert.IsFalse(Exercises5.Ex4(17));
+    }
+
+    [TestMethod]
+    public void Ex5()
+    {
+        Assert.AreEqual(2, Exercises5.Ex5(31, 14));
+        Assert.AreEqual(1, Exercises5.Ex5(0b110, 0b111));
+        Assert.AreEqual(3, Exercises5.Ex5(0b10010, 0b11001));
+        Assert.AreEqual(5, Exercises5.Ex5(0b00000, 0b11111));
+    }
+
+    [TestMethod]
+    public void Ex6()
+    {
+        Assert.AreEqual(0b11011000, Exercises5.Ex6(0b11100100));
+    }
+
+    [TestMethod]
+    public void Ex7()
+    {
+        Assert.AreEqual(3, Exercises5.Ex7(3, (i, j) => j < 2 ? new byte[] { 0, 0, 0, 1, 1, 0 }[i * 2 + j] : (byte)0));
+        Assert.AreEqual(2, Exercises5.Ex7(3, (i, j) => j < 2 ? new byte[] { 0, 0, 1, 1, 1, 0 }[i * 2 + j] : (byte)0));
+        Assert.AreEqual(0, Exercises5.Ex7(3, (i, j) => j < 2 ? new byte[] { 0, 1, 1, 0, 1, 1 }[i * 2 + j] : (byte)0));
+        Assert.AreEqual(1, Exercises5.Ex7(3, (i, j) => j < 2 ? new byte[] { 0, 0, 0, 1, 1, 1 }[i * 2 + j] : (byte)0));
+
+        Assert.AreEqual(7, Exercises5.Ex7(7, (i, j) => j < 3 ? new byte[] { 
+            0, 0, 0, 
+            0, 0, 1, 
+            0, 1, 0, 
+            0, 1, 1, 
+            1, 0, 0, 
+            1, 0, 1, 
+            1, 1, 0 }[i * 3 + j] : (byte)0));
+        Assert.AreEqual(5, Exercises5.Ex7(7, (i, j) => j < 3 ? new byte[] { 
+            0, 0, 0, 
+            0, 0, 1, 
+            0, 1, 0, 
+            0, 1, 1, 
+            1, 0, 0, 
+            1, 1, 0, 
+            1, 1, 1 }[i * 3 + j] : (byte)0));
+
+        Assert.AreEqual(5, Exercises5.Ex7(6, (i, j) => j < 3 ? new byte[] {
+            0, 0, 0,
+            0, 0, 1,
+            0, 1, 0,
+            0, 1, 1,
+            1, 0, 0,
+            1, 1, 0 }[i * 3 + j] : (byte)0));
+
+        Assert.AreEqual(5, Exercises5.Ex7(5, (i, j) => j < 3 ? new byte[] {
+            0, 0, 0,
+            1, 0, 0,
+            0, 1, 0,
+            1, 1, 0,
+            0, 0, 1 }[i * 3 + j] : (byte)0));
+    }
+}
---
+++ CTCI/Exercises5.cs
@@ -0,0 +1,174 @@
+﻿using System.ComponentModel;
+using System.Globalization;
+using System.Text;
+
+namespace CTCI;
+
+public static class Exercises5
+{
+    public static int Ex1(int n, int m, int i, int j)
+    {
+        var mask = (1 << (j - i + 1)) - 1;
+        var r1 = n & ~(mask << i);
+        var r2 = (m & mask) << i;
+        return r1 | r2;
+    }
+
+    public static string Ex2(string decNumberString)
+    {
+        var decimalSeparator = '.';
+        var decNumberStringParts = decNumberString.Split(decimalSeparator);
+
+        if (decNumberStringParts.Length == 1)
+            return new string(IntNumberStringToBinary(decNumberStringParts[0]).ToArray());
+
+        if (decNumberStringParts.Length > 2)
+            return "ERROR";
+
+        var intPart = decNumberStringParts[0];
+        var decimalPart = decNumberStringParts[1];
+
+        try
+        {
+            var binaryIntPart = IntNumberStringToBinary(intPart);
+            var binaryDecimalPart = DecNumberStringToBinary(decimalPart);
+            var binaryString = binaryIntPart.Append(decimalSeparator).Concat(binaryDecimalPart);
+
+            return new string(binaryString.ToArray());
+        }
+        catch (Exception)
+        {
+            return "ERROR";
+        }
+
+        IEnumerable<char> IntNumberStringToBinary(string intNumberString) 
+        {
+            var intValue = long.Parse(intNumberString, CultureInfo.InvariantCulture);
+            var intDigits = new StringBuilder();
+            for (var i = 0; true; i++)
+            {
+                intDigits.Append(intValue & 1);
+                intValue >>= 1;
+                if (intValue == 0)
+                    break;
+            }
+            return intDigits.ToString().Reverse();
+        }
+
+        IEnumerable<char> DecNumberStringToBinary(string decNumberString)
+        {
+            var numberOfDigits = decNumberString.Length;
+            var highestPower = (long)Math.Pow(10, numberOfDigits);
+            var decValue = long.Parse(decNumberString, CultureInfo.InvariantCulture);
+            var decDigits = new StringBuilder();
+            int i;
+            for (i = 1; i < 32; i++)
+            {
+                decValue *= 2;
+                if (decValue >= highestPower)
+                {
+                    decDigits.Append('1');
+                    decValue -= highestPower;
+                }
+                else
+                {
+                    decDigits.Append('0');
+                }
+
+                if (decValue == 0)
+                    break;
+            }
+
+            if (i == 32)
+                throw new Exception("Precision loss");
+
+            return decDigits.ToString();
+        }
+    }
+
+    public static (int, int) Ex3(int n)
+    {
+        if (n == 0) return (int.MinValue, int.MaxValue);
+
+        // Next smaller: find lowest "10" and replace with "01"
+        int i = 0, n1 = n;
+        while (n1 != 0 && (n1 & 0b11) != 0b10)
+        {
+            n1 >>= 1;
+            i++;
+        }
+
+        // "10" not found => all ones => return int.MinValue
+        // "10" found with 0 at position i and 1 at position i + 1 => replace with "01"
+        int nextSmaller = n1 == 0 ? int.MinValue : n & ~(1 << (i + 1)) | (1 << i);
+
+        // Next bigger: find lowest "01" and replace with "10"
+        int j = 0, n2 = n;
+        while ((n2 & 0b11) != 0b01)
+        {
+            n2 >>= 1;
+            j++;
+        }
+
+        // "01" found with 1 at position j and 0 at position j + 1 => replace with "10"
+        int nextBigger = n & ~(1 << j) | (1 << (j + 1));
+
+        return (nextSmaller, nextBigger);
+    }
+
+    public static bool Ex4(int n)
+    {
+        return (n & (n - 1)) == 0;
+    }
+
+    public static int Ex5(int a, int b)
+    {
+        var d = a ^ b;
+        var i = 0;
+        while (d != 0)
+        {
+            i += d & 1;
+            d >>= 1;
+        }
+        return i;
+    }
+
+    public static int Ex6(int n)
+    {
+        int m = 0b01010101_01010101_01010101_01010101;
+        return ((n & m) << 1) | ((n & ~m) >> 1);
+    }
+
+    public static int Ex7(int n, Func<int, int, byte> bit)
+    {
+        if (n == 0)
+            throw new ArgumentException($"{nameof(n)} must be positive.");
+
+        int highestBitSet = 0, n1 = n;
+        while (n1 != 0)
+        {
+            highestBitSet++;
+            n1 >>= 1;
+        }
+
+        int nextPowerOfTwo = 1 << highestBitSet;
+
+        var result = 0;
+        for (var i = n + 1; i < nextPowerOfTwo; i++)
+        {
+            result ^= i;
+        }
+
+        for (var i = 0; i < n; i++)
+        {
+            for (var j = 0; j < 32; j++)
+            {
+                var currentBit = (result & (1 << j)) >> j;
+                var newBit = currentBit ^ bit(i, j);
+                result ^= (-newBit ^ result) & (1 << j);
+            }
+        }
+
+        return result;
+    }
+}
