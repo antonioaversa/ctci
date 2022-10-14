94ee9e9c78a3c681ec9497e6f6a4cfd48cdbeba0
Antonio Aversa <differenziale@gmail.com>
2022-10-14 23:56:25 +0200

    Exercises 8th chapter

---
+++ CTCI.Tests/Exercises8Tests.cs
@@ -0,0 +1,220 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+using System.Text;
+using System.Windows.Markup;
+
+namespace CTCI.Tests;
+
+[TestClass]
+public class Exercises8Tests
+{
+    [TestMethod]
+    public void Ex1_Fibonacci()
+    {
+        Assert.AreEqual(1, Exercises8.Ex1_Fibonacci(1));
+        Assert.AreEqual(2, Exercises8.Ex1_Fibonacci(2));
+        Assert.AreEqual(3, Exercises8.Ex1_Fibonacci(3));
+        Assert.AreEqual(5, Exercises8.Ex1_Fibonacci(4));
+        Assert.AreEqual(8, Exercises8.Ex1_Fibonacci(5));
+    }
+
+    [TestMethod]
+    public void Ex1_FibonacciIterative()
+    {
+        Assert.AreEqual(1, Exercises8.Ex1_FibonacciIterative(1));
+        Assert.AreEqual(2, Exercises8.Ex1_FibonacciIterative(2));
+        Assert.AreEqual(3, Exercises8.Ex1_FibonacciIterative(3));
+        Assert.AreEqual(5, Exercises8.Ex1_FibonacciIterative(4));
+        Assert.AreEqual(8, Exercises8.Ex1_FibonacciIterative(5));
+    }
+
+    [TestMethod]
+    public void Ex2_Count()
+    {
+        Assert.AreEqual(1, Exercises8.Ex2_Count(1));
+        Assert.AreEqual(2, Exercises8.Ex2_Count(2));
+        Assert.AreEqual(6, Exercises8.Ex2_Count(3));
+        Assert.AreEqual(20, Exercises8.Ex2_Count(4));
+    }
+
+    [TestMethod]
+    public void Ex2_CountWithOffLimits()
+    {
+        Assert.AreEqual(1, Exercises8.Ex2_CountWithOffLimits(1, (i, j) => false));
+        Assert.AreEqual(0, Exercises8.Ex2_CountWithOffLimits(1, (i, j) => true));
+        Assert.AreEqual(2, Exercises8.Ex2_CountWithOffLimits(2, (i, j) => false));
+        Assert.AreEqual(0, Exercises8.Ex2_CountWithOffLimits(2, (i, j) => true));
+        Assert.AreEqual(1, Exercises8.Ex2_CountWithOffLimits(2, (i, j) => (i == 1 && j == 2)));
+        Assert.AreEqual(1, Exercises8.Ex2_CountWithOffLimits(2, (i, j) => (i == 2 && j == 1)));
+        Assert.AreEqual(6, Exercises8.Ex2_CountWithOffLimits(3, (i, j) => false));
+        Assert.AreEqual(3, Exercises8.Ex2_CountWithOffLimits(3, (i, j) => (i == 1 && j == 2)));
+        Assert.AreEqual(3, Exercises8.Ex2_CountWithOffLimits(3, (i, j) => (i == 2 && j == 1)));
+        Assert.AreEqual(3, Exercises8.Ex2_CountWithOffLimits(3, (i, j) => (i == 1 && (j == 2 || j == 3))));
+        Assert.AreEqual(0, Exercises8.Ex2_CountWithOffLimits(3, (i, j) => (i == 2 && (j == 1 || j == 2 || j == 3))));
+        Assert.AreEqual(0, Exercises8.Ex2_CountWithOffLimits(3, (i, j) => (i == 3 && (j == 1 || j == 2 || j == 3))));
+    }
+
+    [TestMethod]
+    public void Ex2_PathsWithOffLimits()
+    {
+        // _ x _
+        // _ _ _
+        // _ _ _
+        var paths = Exercises8.Ex2_PathsWithOffLimits(3, (i, j) => (i == 1 && j == 2));
+        Assert.IsTrue(paths.Count(p => p.SequenceEqual(new[] { (1, 1), (2, 1), (2, 2), (2, 3), (3, 3) })) == 1);
+        Assert.IsTrue(paths.Count(p => p.SequenceEqual(new[] { (1, 1), (2, 1), (2, 2), (3, 2), (3, 3) })) == 1);
+        Assert.IsTrue(paths.Count(p => p.SequenceEqual(new[] { (1, 1), (2, 1), (3, 1), (3, 2), (3, 3) })) == 1);
+    }
+
+    [TestMethod]
+    public void Ex3_Subsets()
+    {
+        var set = new HashSet<int> { 1, 2, 3 };
+        var subsets = Exercises8.Ex3_Subsets(set).ToList();
+        Assert.AreEqual(8, subsets.Count);
+        Assert.IsTrue(subsets.Count(s => s.SetEquals(new HashSet<int> { 1, 2, 3 })) == 1);
+        Assert.IsTrue(subsets.Count(s => s.SetEquals(new HashSet<int> { })) == 1);
+        Assert.IsTrue(subsets.Count(s => s.SetEquals(new HashSet<int> { 2, 3 })) == 1);
+    }
+
+    [TestMethod]
+    public void Ex3_SubsetsIterative()
+    {
+        var set = new HashSet<int> { 1, 2, 3 };
+        var subsets = Exercises8.Ex3_SubsetsIterative(set).ToList();
+        Assert.AreEqual(8, subsets.Count);
+        Assert.IsTrue(subsets.Count(s => s.SetEquals(new HashSet<int> { 1, 2, 3 })) == 1);
+        Assert.IsTrue(subsets.Count(s => s.SetEquals(new HashSet<int> { })) == 1);
+        Assert.IsTrue(subsets.Count(s => s.SetEquals(new HashSet<int> { 2, 3 })) == 1);
+    }
+
+    [TestMethod]
+    public void Ex4_AllPermutations()
+    {
+        Assert.IsTrue(Exercises8.Ex4_AllPermutations("abc").ToHashSet().SetEquals(
+            new HashSet<string> { "abc", "acb", "bac", "bca", "cab", "cba" }));
+    }
+
+    [TestMethod]
+    public void Ex5_AllCombinationsOfParenthesesWithTwoStacks()
+    {
+        var combinations1 = Exercises8.Ex5_AllCombinationsOfParenthesesWithTwoStacks(3).ToList();
+        Assert.IsTrue(combinations1.ToHashSet().SetEquals(
+            new HashSet<string> { "()()()", "(())()", "(()())", "()(())", "((()))" }));
+    }
+
+    [TestMethod]
+    public void Ex5_AllCombinationsOfParenthesesConstructive()
+    {
+        var combinations1 = Exercises8.Ex5_AllCombinationsOfParenthesesConstructive(3).ToList();
+        Assert.IsTrue(combinations1.ToHashSet().SetEquals(
+            new HashSet<string> { "()()()", "(())()", "(()())", "()(())", "((()))" }));
+    }
+
+    [TestMethod]
+    public void Ex5_AllCombinationsOfParenthesesChoices()
+    {
+        var combinations1 = Exercises8.Ex5_AllCombinationsOfParenthesesChoices(3).ToList();
+        Assert.IsTrue(combinations1.ToHashSet().SetEquals(
+            new HashSet<string> { "()()()", "(())()", "(()())", "()(())", "((()))" }));
+    }
+
+    [TestMethod]
+    public void Ex6_Paint()
+    {
+        var img1 = StringToImg(@"
+            11111
+            11001
+            00111
+            00000
+            11000");
+        Exercises8.Ex6_Paint(img1, 0, 0, '2');
+        Assert.AreEqual(Normalize(@"
+            22222
+            22002
+            00222
+            00000
+            11000"), Normalize(ImgToString(img1)));
+        Exercises8.Ex6_Paint(img1, 1, 2, '3');
+        Assert.AreEqual(Normalize(@"
+            22222
+            22332
+            00222
+            00000
+            11000"), Normalize(ImgToString(img1)));
+        Exercises8.Ex6_Paint(img1, 4, 4, '4');
+        Assert.AreEqual(Normalize(@"
+            22222
+            22332
+            44222
+            44444
+            11444"), Normalize(ImgToString(img1)));
+
+        var img2 = StringToImg(@"
+            1111101
+            1000101
+            0011111
+            0000001
+            1110001
+            1010000
+            1110000");
+        Exercises8.Ex6_Paint(img2, 1, 4, '2');
+        Assert.AreEqual(Normalize(@"
+            2222202
+            2000202
+            0022222
+            0000002
+            1110002
+            1010000
+            1110000"), Normalize(ImgToString(img2)));
+    }
+
+    private static char[,] StringToImg(string s)
+    {
+        s = Normalize(s);
+        var n = (int)Math.Sqrt(s.Length);
+        var result = new char[n, n];
+        for (var i = 0; i < n; i++)
+            for (var j = 0; j < n; j++)
+                result[i, j] = s[i * n + j];
+        return result;
+    }
+
+    private static string Normalize(string s) =>
+        s.Replace("\n", "").Replace("\r", "").Replace("\t", "").Replace(" ", "");
+
+    private static string ImgToString(char[,] img)
+    {
+        var result = new StringBuilder();
+        for (var i = 0; i < img.GetLength(0); i++)
+        {
+            for (var j = 0; j < img.GetLength(1); j++)
+                result.Append(img[i, j]);
+            result.AppendLine();
+        }
+
+        return result.ToString();
+    }
+
+    [TestMethod]
+    public void Ex7_NCents()
+    {
+        Assert.AreEqual(1, Exercises8.Ex7_NCents(1));
+        Assert.AreEqual(1, Exercises8.Ex7_NCents(2));
+        Assert.AreEqual(1, Exercises8.Ex7_NCents(4));
+        Assert.AreEqual(2, Exercises8.Ex7_NCents(5));
+        Assert.AreEqual(2, Exercises8.Ex7_NCents(7));
+        Assert.AreEqual(4, Exercises8.Ex7_NCents(10));
+        Assert.AreEqual(4, Exercises8.Ex7_NCents(14));
+        Assert.AreEqual(6, Exercises8.Ex7_NCents(15));
+        Assert.AreEqual(9, Exercises8.Ex7_NCents(20));
+    }
+
+    [TestMethod]
+    public void Ex8_NQueens()
+    {
+        var permutations = Exercises8.Ex8_NQueens(3);
+        Trace.WriteLine(string.Join("\n", permutations.Select(
+            permutation => string.Join(" ", permutation.Select(t => $"({t.Item1},{t.Item2})")))));
+    }
+}
---
+++ CTCI/Exercises8.cs
@@ -0,0 +1,301 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+
+namespace CTCI;
+
+public class Exercises8
+{
+    public static int Ex1_Fibonacci(int n) => n <= 1 ? 1 : Ex1_Fibonacci(n - 1) + Ex1_Fibonacci(n - 2);
+
+    public static int Ex1_FibonacciIterative(int n)
+    {
+        if (n <= 1) return 1;
+
+        var last = 1;
+        var secondLast = 1;
+        for (var i = 2; i <= n; i++)
+        {
+            (last, secondLast) = (last + secondLast, last);
+        }
+
+        return last;
+    }
+
+    public static int Ex2_Count(int n)
+    {
+        return Paths(n, n);
+
+        static int Paths(int a, int b)
+        {
+            if (a == 1 && b == 1) return 1;
+            return (b > 1 ? Paths(a, b - 1) : 0) + (a > 1 ? Paths(a - 1, b) : 0);
+        }
+    }
+
+    public static int Ex2_CountWithOffLimits(int n, Func<int, int, bool> offLimits)
+    {
+        return Paths(n, n);
+
+        int Paths(int a, int b)
+        {
+            if (a == 1 && b == 1)
+                return !offLimits(n + 1 - a, n + 1 - b) ? 1 : 0;
+
+            return
+                (b > 1 && !offLimits(n + 1 - a, n + 1 - b + 1) ? Paths(a, b - 1) : 0) +
+                (a > 1 && !offLimits(n + 1 - a + 1, n + 1 - b) ? Paths(a - 1, b) : 0);
+        }
+    }
+
+    public static IEnumerable<IList<(int, int)>> Ex2_PathsWithOffLimits(int n, Func<int, int, bool> offLimits)
+    {
+        return Paths(n, n);
+
+        IEnumerable<IList<(int, int)>> Paths(int a, int b)
+        {
+            var ia = n + 1 - a;
+            var ib = n + 1 - b;
+            if (a == 1 && b == 1)
+            {
+                if (!offLimits(ia, ib))
+                    yield return new List<(int, int)> { (ia, ib) };
+                else 
+                    yield break;
+            }
+
+            var pathsGoingRight = b > 1 && !offLimits(ia, ib + 1) 
+                ? Paths(a, b - 1).Select(p => p.Prepend((ia, ib)).ToList())
+                : Enumerable.Empty<IList<(int, int)>>();
+
+            var pathsGoingDown = a > 1 && !offLimits(ia + 1, ib)
+                ? Paths(a - 1, b).Select(p => p.Prepend((ia, ib)).ToList())
+                : Enumerable.Empty<IList<(int, int)>>();
+
+            foreach (var path in pathsGoingRight.Concat(pathsGoingDown))
+                yield return path;
+        }
+    }
+
+    public static IEnumerable<ISet<T>> Ex3_Subsets<T>(ISet<T> set)
+    {
+        return Subsets(set.ToList(), 0);
+
+        static IEnumerable<ISet<T>> Subsets(IList<T> list, int fromIndex)
+        {
+            if (fromIndex == list.Count)
+            {
+                yield return new HashSet<T> { };
+                yield break;
+            }
+
+            foreach (var subset in Subsets(list, fromIndex + 1))
+            {
+                yield return subset;
+                yield return subset.Append(list[fromIndex]).ToHashSet();
+            }
+        }
+    }
+
+    public static IEnumerable<ISet<T>> Ex3_SubsetsIterative<T>(ISet<T> set)
+    {
+        var list = set.ToList();
+
+        for (var i = 0L; i < 1L << list.Count; i++)
+        {
+            var subset = new HashSet<T> { };
+
+            var j = 0;
+            var k = i;
+            while (k != 0)
+            {
+                if ((k & 1) == 1)
+                    subset.Add(list[j]);
+                j++;
+                k >>= 1;
+            }
+
+            yield return subset;
+        }
+    }
+
+    public static IEnumerable<string> Ex4_AllPermutations(string s)
+    {
+        return AllPermutations(s, 0);
+        
+        static IEnumerable<string> AllPermutations(string s, int fromIndex)
+        {
+            if (fromIndex >= s.Length)
+            {
+                yield return string.Empty;
+                yield break;
+            }
+
+            foreach (var permutation in AllPermutations(s, fromIndex + 1))
+            {
+                for (var i = 0; i < s.Length - fromIndex; i++)
+                    yield return permutation[..i] + s[fromIndex] + permutation[i..];
+            }
+        }
+    }
+
+    public static IEnumerable<string> Ex5_AllCombinationsOfParenthesesWithTwoStacks(int n)
+    {
+        return AllCombinationsOfParentheses(n, Enumerable.Empty<char>(), Enumerable.Empty<char>()).Distinct();
+
+        static IEnumerable<string> AllCombinationsOfParentheses(int n, IEnumerable<char> s1, IEnumerable<char> s2)
+        {
+            if (n == 0)
+            {
+                yield return new string(s1.Concat(s2.Reverse()).ToArray());
+                yield break;
+            }
+
+            var combinations = AllCombinationsOfParentheses(n - 1, s1.Append('('), s2.Append(')'));
+
+            combinations = combinations.Concat(AllCombinationsOfParentheses(n - 1, s1.Append('(').Append(')'), s2));
+            combinations = combinations.Concat(AllCombinationsOfParentheses(n - 1, s1, s2.Append(')').Append('(')));
+
+            foreach (var combination in combinations)
+                yield return combination;
+        }
+    }
+
+    public static IEnumerable<string> Ex5_AllCombinationsOfParenthesesConstructive(int n)
+    {
+        return AllCombinationsOfParentheses(n).Distinct();
+
+        static IEnumerable<string> AllCombinationsOfParentheses(int n)
+        {
+            if (n == 0)
+            {
+                yield return string.Empty;
+                yield break;
+            }
+
+            foreach (var combination in AllCombinationsOfParentheses(n - 1))
+            {
+                yield return combination + "()";
+                yield return "(" + combination + ")";
+                yield return "()" + combination;
+            }
+        }
+    }
+
+    public static IEnumerable<string> Ex5_AllCombinationsOfParenthesesChoices(int n)
+    {
+        return AllCombinationsOfParenthesesChoices(n, 0, 0);
+
+        static IEnumerable<string> AllCombinationsOfParenthesesChoices(int n, int opened, int closed)
+        {
+            if (n == closed)
+            {
+                yield return string.Empty;
+                yield break;
+            }
+            
+            if (opened < n)
+                foreach (var s in AllCombinationsOfParenthesesChoices(n, opened + 1, closed))
+                    yield return "(" + s;
+
+            if (closed < opened)
+                foreach (var s in AllCombinationsOfParenthesesChoices(n, opened, closed + 1))
+                    yield return ")" + s;
+        }
+    }
+
+    public static void Ex6_Paint(char[,] img, int i, int j, char newColor)
+    {
+        var X = img.GetLength(0);
+        var Y = img.GetLength(1);
+        var oldColor = img[i, j];
+
+        Paint(i, j);
+
+        void Paint(int i, int j)
+        {
+            if (i >= X || j >= Y || i < 0 || j < 0)
+                return;
+
+            if (img[i, j] != oldColor)
+                return;
+
+            img[i, j] = newColor;
+            Paint(i + 1, j);
+            Paint(i - 1, j);
+            Paint(i, j + 1);
+            Paint(i, j - 1);
+        }
+
+
+        // Paint(i, j, 0, X - 1, 0, Y - 1);
+
+        //void Paint(int i, int j, int min_i, int max_i, int min_j, int max_j)
+        //{
+        //    if (i > max_i || j >= max_j || i < min_i || j < min_j )
+        //        return;
+        //    if (img[i, j] != oldColor)
+        //        return;
+
+        //    img[i, j] = newColor;
+        //    Paint(i + 1, j, i + 1, max_i, min_j, max_j);
+        //    Paint(i - 1, j, min_i, i - 1, min_j, max_j);
+        //    Paint(i, j + 1, min_i, max_i, j + 1, max_j);
+        //    Paint(i, j - 1, min_i, max_j, min_j, j + 1);
+        //}
+    }
+
+    public static int Ex7_NCents(int n)
+    {
+        var tokens = new int[] { 25, 10, 5, 1 };
+        return NCents(n, 0);
+
+        int NCents(int n, int maxTokenIndex)
+        {
+            if (n == 0)
+                return 1;
+            if (maxTokenIndex >= tokens.Length)
+                return -1;
+
+            var result = 0;
+            for (var i = 0; i <= n / tokens[maxTokenIndex]; i++)
+            {
+                var ways = NCents(n - i * tokens[maxTokenIndex], maxTokenIndex + 1);
+                if (ways < 0)
+                    continue;
+                result += ways;
+            }
+            return result;
+        }
+    }
+
+    public static IEnumerable<IList<(int, int)>> Ex8_NQueens(int n)
+    {
+        return NQueens(n, new List<(int, int)> { });
+
+        static IEnumerable<IList<(int, int)>> NQueens(int n, IList<(int, int)> placedQueens)
+        {
+            if (placedQueens.Count == n)
+            {
+                yield return placedQueens;
+                yield break;
+            }
+
+            var i = placedQueens.Count;
+            for (var j = 0; j < n; j++)
+            {
+                if (AreCompatible(placedQueens, i, j))
+                {
+                    var placedQueensWithOneMore = placedQueens.Append((i, j)).ToList();
+                    foreach (var way in NQueens(n, placedQueensWithOneMore))
+                        yield return way;
+                }
+            }
+        }
+
+        static bool AreCompatible(IList<(int, int)> placedQueens, int i, int j)
+        {
+            return placedQueens.All(
+                c => c.Item1 != i && c.Item2 != j && c.Item1 - c.Item2 != i - j && c.Item1 + c.Item2 != i + j);
+        }
+    }
+}
