d38f329e991f446468159fbb1329dbd1a421ad21
Antonio Aversa <differenziale@gmail.com>
2022-10-18 13:57:26 +0200

    Exercises 6th chapter

---
+++ CTCI.Tests/Exercises13Tests.cs
@@ -0,0 +1,21 @@
+﻿namespace CTCI.Tests;
+
+using static CTCI.Exercises13;
+
+[TestClass]
+public class Exercises13Tests
+{
+    [TestMethod]
+    public void Ex8_Copy()
+    {
+        var node1 = new Node(null, null);
+        var node2 = new Node(node1, node1);
+        var node3 = new Node(node2, node2);
+        var copy3 = Exercises13.Ex8_Copy(node3);
+        Assert.IsNotNull(copy3);
+        Assert.AreSame(copy3.Left, copy3.Right);
+        Assert.AreSame(copy3.Left?.Left, copy3.Left?.Right);
+        Assert.AreSame(copy3.Right?.Left, copy3.Right?.Right);
+        Assert.AreSame(copy3.Left?.Left, copy3.Right?.Left);
+    }
+}
---
+++ CTCI/Exercises13.cs
@@ -0,0 +1,52 @@
+﻿namespace CTCI;
+
+public static class Exercises13
+{
+    private class A 
+    {
+        public virtual int F(int a) => 1;
+        public virtual int F(int a, int b) => 2;
+    }
+
+    private class B: A 
+    {
+        public override int F(int a) => 3;
+    }
+
+    public static void Ex6_VirtualAndOverloads()
+    {
+        A a = new A();
+        a.F(10);
+        a.F(10, 20);
+
+        B b = new B();
+        b.F(10);
+        b.F(10, 20);
+
+        A bAsA = new B();
+        b.F(10);
+        b.F(10, 20);
+    }
+
+    public record Node(Node? Left, Node? Right);
+
+    public static Node Ex8_Copy(Node node)
+    {
+        return Copy(node, new Dictionary<Node, Node> { })!;
+
+        static Node? Copy(Node? node, IDictionary<Node, Node> copies)
+        {
+            if (node == null) 
+                return null;
+            if (copies.TryGetValue(node, out var copy)) 
+                return copy;
+            
+            var leftChildCopy = Copy(node.Left, copies);
+            var rightChildCopy = Copy(node.Right, copies);
+            copy = new Node(leftChildCopy, rightChildCopy);
+            
+            copies[node] = copy;
+            return copy;
+        }
+    }
+}
