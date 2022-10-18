4c4ec0de2ab4416a3f66cd0d7f8f758fd507279b
Antonio Aversa <differenziale@gmail.com>
2022-10-18 21:10:28 +0200

    Leetcode: 5, 22, 45

---
+++ CTCI.Tests/DynamicProgrammingTests.cs
@@ -0,0 +1,63 @@
+﻿using CTCI;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CTCI.Tests;
+
+[TestClass]
+public class DynamicProgrammingTests
+{
+    [TestMethod]
+    public void Ex5_LongestPalindrome()
+    {
+        Assert.AreEqual("bbb", DynamicProgramming.Ex5_LongestPalindrome("bbb"));
+        Assert.AreEqual("bbb", DynamicProgramming.Ex5_LongestPalindrome("abbb"));
+        Assert.AreEqual("bbb", DynamicProgramming.Ex5_LongestPalindrome("bbba"));
+        Assert.AreEqual("aaaa", DynamicProgramming.Ex5_LongestPalindrome("bbbaaaacbb"));
+        Assert.AreEqual("bbbaaaabbb", DynamicProgramming.Ex5_LongestPalindrome("bbbaaaabbb"));
+        Assert.AreEqual("bbbb", DynamicProgramming.Ex5_LongestPalindrome("bbbb"));
+        Assert.AreEqual("bab", DynamicProgramming.Ex5_LongestPalindrome("babad"));
+        Assert.AreEqual("bb", DynamicProgramming.Ex5_LongestPalindrome("cbbd"));
+    }
+
+    [TestMethod]
+    public void Ex22_GenerateParenthesis()
+    {
+        Assert.IsTrue(new HashSet<string> {
+            "()()()", "()(())", "(())()", "(()())", "((()))" }
+            .SetEquals(DynamicProgramming.Ex22_GenerateParenthesis(3)));
+
+        Assert.IsTrue(new HashSet<string> { 
+            "()()()()", "()()(())", "()(())()", "()(()())", "()((()))", "(())()()", "(())(())", "(()())()", "(()()())", 
+            "(()(()))", "((()))()", "((())())", "((()()))", "(((())))" }
+            .SetEquals(DynamicProgramming.Ex22_GenerateParenthesis(4)));
+    }
+
+    [TestMethod]
+    public void Ex45_Jump()
+    {
+        Assert.AreEqual(2, DynamicProgramming.Ex45_Jump(new int[] { 2, 3, 1, 9, 4 }));
+        Assert.AreEqual(4, DynamicProgramming.Ex45_Jump(new int[] { 2, 3, 1, 1, 1, 9, 4 }));
+        Assert.AreEqual(2, DynamicProgramming.Ex45_Jump(new int[] { 2, 3, 0, 1, 4 }));
+    }
+
+    [TestMethod]
+    public void Ex45_JumpFaster()
+    {
+        Assert.AreEqual(2, DynamicProgramming.Ex45_JumpFaster(new int[] { 2, 3, 1, 9, 4 }));
+        Assert.AreEqual(4, DynamicProgramming.Ex45_JumpFaster(new int[] { 2, 3, 1, 1, 1, 9, 4 }));
+        Assert.AreEqual(2, DynamicProgramming.Ex45_JumpFaster(new int[] { 2, 3, 0, 1, 4 }));
+    }
+
+    [TestMethod]
+    public void Ex45_JumpFastest()
+    { 
+        Assert.AreEqual(2, DynamicProgramming.Ex45_JumpFastest(new int[] { 2, 3, 1, 9, 4 }));
+        Assert.AreEqual(4, DynamicProgramming.Ex45_JumpFastest(new int[] { 2, 3, 1, 1, 1, 9, 4 }));
+        Assert.AreEqual(2, DynamicProgramming.Ex45_JumpFastest(new int[] { 2, 3, 0, 1, 4 }));
+    }
+}
---
+++ CTCI/DynamicProgramming.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CTCI;
+
+public static class DynamicProgramming
+{
+    public static string Ex5_LongestPalindrome(string s)
+    {
+        var solutions = new Dictionary<(int, int), (int, int)> { };
+
+        var (start, count) = LongestPalindrome(0, s.Length);
+        return s[start..(start + count)];
+
+        (int start, int count) LongestPalindrome(int start, int count)
+        {
+            if (count <= 1) return (start, count);
+            if (count == 2) return s[start] == s[start + 1] ? (start, count) : (start, 1);
+            if (solutions.TryGetValue((start, count), out var solution)) return solution;
+
+            if (s[start] == s[start + count - 1])
+            {
+                var (start3, count3) = LongestPalindrome(start + 1, count - 2);
+                if (count3 == count - 2)
+                    return (start, count);
+            }
+
+            var (start1, count1) = LongestPalindrome(start, count - 1);
+            if (count1 == count - 1)
+                return (start1, count1);
+
+            var (start2, count2) = LongestPalindrome(start + 1, count - 1);
+            solution = count1 >= count2 ? (start1, count1) : (start2, count2);
+            solutions[(start, count)] = solution;
+            return solution;
+        }
+    }
+
+    public static IList<string> Ex22_GenerateParenthesis(int n)
+    {
+        return GenerateParenthesis(0, 0).ToList();
+
+        IEnumerable<string> GenerateParenthesis(int opened, int closed)
+        {
+            if (closed == n) { yield return string.Empty; yield break; }
+            if (opened == n) { yield return new string(')', n - closed); yield break; }
+            if (closed < opened)
+                foreach (var s in GenerateParenthesis(opened, closed + 1))
+                    yield return ')' + s;
+            foreach (var s in GenerateParenthesis(opened + 1, closed))
+                yield return '(' + s;
+        }
+    }
+
+    public static int Ex45_Jump(int[] nums)
+    {
+        var n = nums.Length;
+        var solutions = new Dictionary<int, int> { };
+        return Jump(0);
+
+        int Jump(int p)
+        {
+            if (p == n - 1) return 0;
+            if (solutions.TryGetValue(p, out var solution)) return solution;
+
+            solution = int.MaxValue;
+
+            for (var i = 1; i <= nums[p] && p + i < n; i++)
+            {
+                var jumps = Jump(p + i);
+                if (jumps != int.MaxValue && jumps + 1 < solution)
+                    solution = 1 + jumps;
+            }
+
+            solutions[p] = solution;
+            return solution;
+        }
+    }
+
+    public static int Ex45_JumpFaster(int[] nums)
+    {
+        var n = nums.Length;
+        var solutions = new int[n - 1];
+        return Jump(0);
+
+        int Jump(int p)
+        {
+            if (p == n - 1) return 0;
+            if (solutions[p] != 0) return solutions[p];
+
+            var solution = int.MaxValue;
+
+            for (var i = 1; i <= nums[p] && p + i < n; i++)
+            {
+                var jumps = Jump(p + i);
+                if (jumps != int.MaxValue && jumps + 1 < solution)
+                    solution = 1 + jumps;
+            }
+
+            solutions[p] = solution;
+            return solution;
+        }
+    }
+
+    public static int Ex45_JumpFastest(int[] nums)
+    {
+        var farthest = 0;
+        var jumpMax = 0;
+        var breath = 0;
+        for (var i = 0; i < nums.Length - 1; i++)
+        {
+            farthest = Math.Max(farthest, i + nums[i]);
+
+            if (i == jumpMax)
+            {
+                breath++;
+                jumpMax = farthest; 
+            }
+        }
+
+        return breath;
+    }
+}
